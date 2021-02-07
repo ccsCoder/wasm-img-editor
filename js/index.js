4eb2367a43d40321ef43d5da95cc339d5876ca17
Nishant <ccsCoder@gmail.com>
2021-02-07 12:52:29 +0530

    toolbar controls

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+// Copied shamelessly from https://animate.style/#documentation
+const animate = async (node, animation, prefix = 'animate__') => {
+  return new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    function onAnimationEnd() {
+      resolve(node);
+    }
+    node.addEventListener('animationend', onAnimationEnd);
+    node.classList.remove(animationName);
+    node.classList.add(`${prefix}animated`, animationName);
+  });
+};
+
+
 function init() {
   initFileuploader();
+  initEventHandlers();
+}
+
+function initEventHandlers() {
+  document.querySelector('#image-edit-button').addEventListener('click', e => {
+    document.querySelector('#file-selection').click();
+  });
 }
 
 function initFileuploader() {
@@ -10,18 +31,26 @@ function initFileuploader() {
   });
 
   // On file selected.
-  document.querySelector('#file-selection').addEventListener('change', function() {
-    if (!this.files || !this.files[0]) return;
+  document.querySelector('#file-selection').addEventListener('change', onFileSelected);
+}
+
+function onFileSelected() {
+    if (!this.files || !this.files[0])
+      return;
     // hide header
     toggleToolbar();
     // otherwise display stuff
     displayFilename(this.files[0].name);
     previewImage(this.files[0]);
-  });
 }
 
-function toggleToolbar() {
-  document.querySelector('.image-preview > h3').style.display = 'none';
+async function toggleToolbar() {
+  document.querySelector('.image-preview > h3').classList.add('hidden');
+  const fileSelector = await animate(document.querySelector('.file-selection'), 'fadeOutUp');
+  fileSelector.classList.add('hidden');
+  const toolbar = document.querySelector('.image-meta');
+  toolbar.classList.remove('hidden');
+  animate(toolbar, 'slideInLeft');
 }
 
 function setImageSource({ target: { result }}) {
